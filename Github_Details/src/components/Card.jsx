9b10728c3788bc87760bb6caf24bbf4083b26072
Aakarbharadwaj <104188303+Aakarbharadwaj@users.noreply.github.com>
2024-02-08 01:51:25 +0530

    fetching git api

--- Github_Details/src/components/Card.jsx
+++ Github_Details/src/components/Card.jsx
@@ -3,21 +3,22 @@ import './Card.css'
 const Card = () => {
     const [data, setdata] = useState([]);
     useEffect(() => {
-        const fetchdata= async () => {
+        const fetchdata = async () => {
             let res = await fetch("https://api.github.com/users/aakarbharadwaj");
             let info = await res.json();
             console.log(info)
             setdata(info);
         }
         fetchdata();
-    })
+    }, [])
     return (
         <div className="card" style={{ width: '18rem' }}>
-            <img src="..." className="card-img-top" alt="..." />
+            <img src={data.avatar_url} className="card-img-top" alt="..." />
             <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+                <h5 className="card-title">{data.name}</h5>
+                <p className="card-text bg-primary text-light rounded Follower">Follower's {data.followers}</p>
+                <a href={data.html_url} className="btn btn-primary">Go to Git Profile</a>
+                <a href={data.html_url} className="btn border text-primary">Go to linkedIn Profile</a>
             </div>
         </div>
     )
